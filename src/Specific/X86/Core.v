d221b53e77f76e8830dd8ba8d001a089a48eac00
Jason Gross <jgross@mit.edu>
2016-10-12 15:47:27 -0400

    WIP

---
+++ src/Specific/X86/Core.v
@@ -0,0 +1,281 @@
+(** * An x86 Machine *)
+Require Import Coq.Classes.RelationClasses Coq.Classes.Morphisms.
+Require Import Coq.PArith.BinPos Coq.micromega.Psatz.
+Require Export Coq.ZArith.ZArith Coq.Lists.List.
+Require Import Crypto.Util.Decidable.
+Require Export Crypto.BoundedArithmetic.Interface.
+Require Export Crypto.BoundedArithmetic.ArchitectureToZLike.
+Require Export Crypto.BoundedArithmetic.ArchitectureToZLikeProofs.
+Require Export Crypto.Util.Tuple.
+Require Import Crypto.Util.Option Crypto.Util.Sigma Crypto.Util.Prod.
+Require Export Crypto.Reflection.Named.Syntax.
+Require Import Crypto.Reflection.Named.DeadCodeElimination.
+Require Import Crypto.Reflection.CountLets.
+Require Import Crypto.Reflection.Named.ContextOn.
+Require Export Crypto.Reflection.Syntax.
+Require Import Crypto.Reflection.Linearize.
+Require Import Crypto.Reflection.Inline.
+Require Import Crypto.Reflection.CommonSubexpressionElimination.
+Require Export Crypto.Reflection.Reify.
+Require Export Crypto.Util.ZUtil.
+Require Export Crypto.Util.Notations.
+Require Import Crypto.Util.ListUtil.
+Require Export Crypto.Util.LetIn.
+Export ListNotations.
+
+Open Scope Z_scope.
+Local Notation eta x := (fst x, snd x).
+Local Notation eta3 x := (eta (fst x), snd x).
+Local Notation eta3' x := (fst x, eta (snd x)).
+
+(** ** Reflective Assembly Syntax *)
+Section reflection.
+  Context {n} (ops : x86.instructions n).
+  Local Set Boolean Equality Schemes.
+  Local Set Decidable Equality Schemes.
+  Inductive base_type := TZ | Tbool | TW.
+  Global Instance dec_base_type : DecidableRel (@eq base_type)
+    := base_type_eq_dec.
+  Definition interp_base_type (v : base_type) : Type :=
+    match v with
+    | TZ => Z
+    | Tbool => bool
+    | TW => x86.W
+    end.
+  Local Notation tZ := (Tbase TZ).
+  Local Notation tbool := (Tbase Tbool).
+  Local Notation tW := (Tbase TW).
+  Local Open Scope ctype_scope.
+  Print x86.instructions.
+  Inductive op : flat_type base_type -> flat_type base_type -> Type :=
+  | OPldi     : op tZ tW
+  | OPshrd    : op (tW * tW * tZ) (tbool * tW)
+  | OPshl     : op (tW * tZ) (tbool * tW)
+  | OPshr     : op (tW * tZ) (tbool * tW)
+  | OPadc     : op (tW * tW * tbool) (tbool * tW)
+  | OPsubc    : op (tW * tW * tbool) (tbool * tW)
+  | OPmuldw   : op (tW * tW) (tbool * (tW * tW))
+  | OPselc    : op (tbool * tW * tW) tW
+  | OPor      : op (tW * tW) (tbool * tW).
+
+  Definition interp_op src dst (f : op src dst)
+    : interp_flat_type_gen interp_base_type src -> interp_flat_type_gen interp_base_type dst
+    := match f in op s d return interp_flat_type_gen _ s -> interp_flat_type_gen _ d with
+       | OPldi     => ldi
+       | OPshrd    => fun xyz => let '(x, y, z) := eta3 xyz in shrdf x y z
+       | OPshl     => fun xy => let '(x, y) := eta xy in shlf x y
+       | OPshr     => fun xy => let '(x, y) := eta xy in shrf x y
+       | OPadc     => fun xyz => let '(x, y, z) := eta3 xyz in adc x y z
+       | OPsubc    => fun xyz => let '(x, y, z) := eta3 xyz in subc x y z
+       | OPmuldw   => fun xy => let '(x, y) := eta xy in muldwf x y
+       | OPselc    => fun xyz => let '(x, y, z) := eta3 xyz in selc x y z
+       | OPor      => fun xy => let '(x, y) := eta xy in orf x y
+       end.
+
+  Definition Inline {t} e := @InlineConst base_type interp_base_type op t e.
+End reflection.
+
+Ltac base_reify_op op op_head ::=
+     lazymatch op_head with
+     | @Interface.ldi => constr:(reify_op op op_head 1 OPldi)
+     | @Interface.shrdf => constr:(reify_op op op_head 3 OPshrd)
+     | @Interface.shlf => constr:(reify_op op op_head 2 OPshl)
+     | @Interface.shrf => constr:(reify_op op op_head 2 OPshr)
+     | @Interface.adc => constr:(reify_op op op_head 3 OPadc)
+     | @Interface.subc => constr:(reify_op op op_head 3 OPsubc)
+     | @Interface.muldwf => constr:(reify_op op op_head 2 OPmuldw)
+     | @Interface.selc => constr:(reify_op op op_head 3 OPselc)
+     | @Interface.orf => constr:(reify_op op op_head 3 OPor)
+     end.
+Ltac base_reify_type T ::=
+     match T with
+     | Z => TZ
+     | bool => Tbool
+     | x86.W => TW
+     end.
+
+Ltac Reify' e := Reify.Reify' base_type (interp_base_type _) op e.
+Ltac Reify e :=
+  let v := Reify.Reify base_type (interp_base_type _) op e in
+  constr:(Inline _ ((*CSE _*) (InlineConst (Linearize v)))).
+(*Ltac Reify_rhs := Reify.Reify_rhs base_type (interp_base_type _) op (interp_op _).*)
+
+(*
+(** ** Raw Syntax Trees *)
+(** These are used solely for pretty-printing the expression tree in a
+    form that can be basically copy-pasted into other languages which
+    can be compiled for the Fancy Machine.  Hypothetically, we could
+    add support for custom named identifiers, by carrying around
+    [string] identifiers and using them for pretty-printing...  It
+    might also be possible to verify this layer, too, by adding a
+    partial interpretation function... *)
+
+Local Set Decidable Equality Schemes.
+Local Set Boolean Equality Schemes.
+
+Inductive Register :=
+| RegPInv | RegMod | RegMuLow | RegZero
+| y | t1 | t2 | lo | hi | out | src1 | src2 | tmp | q | qHigh | x | xHigh
+| SpecialCarryBit
+| scratch | scratchplus (n : nat).
+
+Notation "'scratch+' n" := (scratchplus n) (format "'scratch+' n", at level 10).
+
+Definition pos_of_Register (r : Register) :=
+  match r with
+  | RegPInv => 1
+  | RegMod => 2
+  | RegMuLow => 3
+  | RegZero => 4
+  | y => 5
+  | t1 => 6
+  | t2 => 7
+  | lo => 8
+  | hi => 9
+  | out => 10
+  | src1 => 11
+  | src2 => 12
+  | tmp => 13
+  | q => 14
+  | qHigh => 15
+  | x => 16
+  | xHigh => 17
+  | SpecialCarryBit => 18
+  | scratch => 19
+  | scratchplus n => 19 + Pos.of_nat (S n)
+  end%positive.
+
+Lemma pos_of_Register_inj x y : pos_of_Register x = pos_of_Register y -> x = y.
+Proof.
+  unfold pos_of_Register; repeat break_match; subst;
+    try rewrite Pos.add_cancel_l; try rewrite Nat2Pos.inj_iff;
+      try solve [ simpl; congruence | intros; exfalso; lia ].
+Qed.
+
+Global Instance RegisterContext {var : base_type -> Type} : Context Register var
+  := ContextOn pos_of_Register (RegisterAssign.pos_context var).
+
+Definition syntax {ops : fancy_machine.instructions (2 * 128)}
+  := Named.expr base_type (interp_base_type ops) op Register.
+
+Class wf_empty {ops} {var} {t} (e : Named.expr base_type (interp_base_type ops) op Register t)
+  := mk_wf_empty : @Named.wf base_type (interp_base_type ops) op Register var _ empty t e.
+Global Hint Extern 0 (wf_empty _) => vm_compute; intros; constructor : typeclass_instances.
+
+(** Assemble a well-typed easily interpretable expression into a
+    syntax tree we can use for pretty-printing. *)
+Section assemble.
+  Context {ops : fancy_machine.instructions (2 * 128)}.
+
+  Definition AssembleSyntax' {t} (e : Expr base_type (interp_base_type _) op t) (ls : list Register)
+    : option (syntax t)
+    := CompileAndEliminateDeadCode e ls.
+  Definition AssembleSyntax {t} e ls (res := @AssembleSyntax' t e ls)
+    := match res return match res with None => _ | _ => _ end with
+       | Some v => v
+       | None => I
+       end.
+
+  Definition dummy_registers (n : nat) : list Register
+    := List.map scratchplus (seq 0 n).
+  Definition DefaultRegisters {t} (e : Expr base_type (interp_base_type _) op t) : list Register
+    := dummy_registers (CountBinders e).
+
+  Definition DefaultAssembleSyntax {t} e := @AssembleSyntax t e (DefaultRegisters e).
+
+  Definition Interp {t} e {wf : wf_empty e}
+    := @Named.interp base_type (interp_base_type _) op Register _ (interp_op _) empty t e wf.
+End assemble.
+
+Export Reflection.Named.Syntax.
+Open Scope nexpr_scope.
+Open Scope ctype_scope.
+Open Scope type_scope.
+Open Scope core_scope.
+
+Notation Return x := (Var x).
+Notation ldi z := (Op OPldi (Const z%Z)).
+Notation "'slet' x := A 'in' b" := (LetIn _ x (Op OPldi (Var A%nexpr)) b%nexpr) : nexpr_scope.
+Notation "'c.Rshi' ( x , A , B , C ) , b" :=
+  (LetIn _ x (Op OPshrd (Pair (Pair (Var A) (Var B)) (Const C%Z))) b)
+    (at level 200, b at level 200, format "'c.Rshi' ( x ,  A ,  B ,  C ) , '//' b").
+Notation "'c.Mul128' ( x , 'c.UpperHalf' ( A ) , 'c.UpperHalf' ( B ) ) , b" :=
+  (LetIn _ x (Op OPmulhwhh (Pair (Var A) (Var B))) b)
+    (at level 200, b at level 200, format "'c.Mul128' ( x ,  'c.UpperHalf' ( A ) ,  'c.UpperHalf' ( B ) ) , '//' b").
+Notation "'c.Mul128' ( x , 'c.UpperHalf' ( A ) , 'c.LowerHalf' ( B ) ) , b" :=
+  (LetIn _ x (Op OPmulhwhl (Pair (Var A) (Var B))) b)
+    (at level 200, b at level 200, format "'c.Mul128' ( x ,  'c.UpperHalf' ( A ) ,  'c.LowerHalf' ( B ) ) , '//' b").
+Notation "'c.Mul128' ( x , 'c.LowerHalf' ( A ) , 'c.LowerHalf' ( B ) ) , b" :=
+  (LetIn _ x (Op OPmulhwll (Pair (Var A) (Var B))) b)
+    (at level 200, b at level 200, format "'c.Mul128' ( x ,  'c.LowerHalf' ( A ) ,  'c.LowerHalf' ( B ) ) , '//' b").
+Notation "'c.LeftShifted' { x , v }" :=
+  (Op OPshl (Pair (Var x) (Const v%Z)))
+    (at level 200, format "'c.LeftShifted' { x ,  v }").
+Notation "'c.RightShifted' { x , v }" :=
+  (Op OPshr (Pair (Var x) (Const v%Z)))
+    (at level 200, format "'c.RightShifted' { x ,  v }").
+
+Notation "'c.Add' ( x , A , B ) , b" :=
+  (LetIn _ (pair SpecialCarryBit x) (Op OPadc (Pair (Pair A B) (Const false))) b)
+    (at level 200, b at level 200, format "'c.Add' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Add' ( x , A , B ) , b" :=
+  (LetIn _ (pair SpecialCarryBit x) (Op OPadc (Pair (Pair (Var A) B) (Const false))) b)
+    (at level 200, b at level 200, format "'c.Add' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Add' ( x , A , B ) , b" :=
+  (LetIn _ (pair SpecialCarryBit x) (Op OPadc (Pair (Pair A (Var B)) (Const false))) b)
+    (at level 200, b at level 200, format "'c.Add' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Add' ( x , A , B ) , b" :=
+  (LetIn _ (pair SpecialCarryBit x) (Op OPadc (Pair (Pair (Var A) (Var B)) (Const false))) b)
+    (at level 200, b at level 200, format "'c.Add' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Addc' ( x , A , B ) , b" :=
+  (LetIn _ (pair SpecialCarryBit x) (Op OPadc (Pair (Pair A B) (Var SpecialCarryBit))) b)
+    (at level 200, b at level 200, format "'c.Addc' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Addc' ( x , A , B ) , b" :=
+  (LetIn _ (pair SpecialCarryBit x) (Op OPadc (Pair (Pair (Var A) B) (Var SpecialCarryBit))) b)
+    (at level 200, b at level 200, format "'c.Addc' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Addc' ( x , A , B ) , b" :=
+  (LetIn _ (pair SpecialCarryBit x) (Op OPadc (Pair (Pair A (Var B)) (Var SpecialCarryBit))) b)
+    (at level 200, b at level 200, format "'c.Addc' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Addc' ( x , A , B ) , b" :=
+  (LetIn _ (pair SpecialCarryBit x) (Op OPadc (Pair (Pair (Var A) (Var B)) (Var SpecialCarryBit))) b)
+    (at level 200, b at level 200, format "'c.Addc' ( x ,  A ,  B ) , '//' b").
+
+Notation "'c.Sub' ( x , A , B ) , b" :=
+  (LetIn _ (pair SpecialCarryBit x) (Op OPsubc (Pair (Pair A B) (Const false))) b)
+    (at level 200, b at level 200, format "'c.Sub' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Sub' ( x , A , B ) , b" :=
+  (LetIn _ (pair SpecialCarryBit x) (Op OPsubc (Pair (Pair (Var A) B) (Const false))) b)
+    (at level 200, b at level 200, format "'c.Sub' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Sub' ( x , A , B ) , b" :=
+  (LetIn _ (pair SpecialCarryBit x) (Op OPsubc (Pair (Pair A (Var B)) (Const false))) b)
+    (at level 200, b at level 200, format "'c.Sub' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Sub' ( x , A , B ) , b" :=
+  (LetIn _ (pair SpecialCarryBit x) (Op OPsubc (Pair (Pair (Var A) (Var B)) (Const false))) b)
+    (at level 200, b at level 200, format "'c.Sub' ( x ,  A ,  B ) , '//' b").
+
+Notation "'c.Addm' ( x , A , B ) , b" :=
+  (LetIn _ x (Op OPaddm (Pair (Pair A B) (Var RegMod))) b)
+    (at level 200, b at level 200, format "'c.Addm' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Addm' ( x , A , B ) , b" :=
+  (LetIn _ x (Op OPaddm (Pair (Pair (Var A) B) (Var RegMod))) b)
+    (at level 200, b at level 200, format "'c.Addm' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Addm' ( x , A , B ) , b" :=
+  (LetIn _ x (Op OPaddm (Pair (Pair A (Var B)) (Var RegMod))) b)
+    (at level 200, b at level 200, format "'c.Addm' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Addm' ( x , A , B ) , b" :=
+  (LetIn _ x (Op OPaddm (Pair (Pair (Var A) (Var B)) (Var RegMod))) b)
+    (at level 200, b at level 200, format "'c.Addm' ( x ,  A ,  B ) , '//' b").
+
+Notation "'c.Selc' ( x , A , B ) , b" :=
+  (LetIn _ x (Op OPselc (Pair (Pair (Var SpecialCarryBit) A) B)) b)
+    (at level 200, b at level 200, format "'c.Selc' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Selc' ( x , A , B ) , b" :=
+  (LetIn _ x (Op OPselc (Pair (Pair (Var SpecialCarryBit) (Var A)) B)) b)
+    (at level 200, b at level 200, format "'c.Selc' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Selc' ( x , A , B ) , b" :=
+  (LetIn _ x (Op OPselc (Pair (Pair (Var SpecialCarryBit) A) (Var B))) b)
+    (at level 200, b at level 200, format "'c.Selc' ( x ,  A ,  B ) , '//' b").
+Notation "'c.Selc' ( x , A , B ) , b" :=
+  (LetIn _ x (Op OPselc (Pair (Pair (Var SpecialCarryBit) (Var A)) (Var B))) b)
+    (at level 200, b at level 200, format "'c.Selc' ( x ,  A ,  B ) , '//' b").
+*)
--- src/Specific/X86/Exponent25519.v
+++ src/Specific/X86/Exponent25519.v
@@ -1,5 +1,6 @@
-Require Import Coq.ZArith.ZArith.
+Require Import Coq.ZArith.ZArith Coq.micromega.Psatz.
 Require Import Crypto.Spec.Ed25519.
+Require Import Crypto.Specific.X86.Core.
 Require Import Crypto.EdDSARepChange.
 Require Import Crypto.BoundedArithmetic.Interface.
 Require Import Crypto.BoundedArithmetic.X86ToZLike.
@@ -7,30 +8,188 @@ Require Import Crypto.BoundedArithmetic.X86ToZLikeProofs.
 Require Import Crypto.ModularArithmetic.BarrettReduction.ZBounded.
 Require Import Crypto.Util.Tuple.
 Require Import Crypto.Util.LetIn.
+Import NPeano.
 
-Local Notation modulus := (2^252 + 27742317777372353535851937790883648493)%Z.
+Local Notation modulusv := (2^252 + 27742317777372353535851937790883648493)%Z.
 Local Coercion Z.of_nat : nat >-> Z.
 Local Notation eta x := (fst x, snd x).
 Local Notation eta3 x := (eta (fst x), snd x).
 Local Notation eta4 x := (eta3 (fst x), snd x).
 Local Notation eta4' x := (eta (fst x), eta (snd x)).
 
-Section x86_64.
-  Local Notation n := 64%nat.
+Section x86.
   Local Notation base := 2%Z (* TODO(@andres-erbsen): Is this the correct base, or are we using something else? *).
   Local Notation smaller_bound_exp := 250%Z (* TODO(@andres-erbsen): Is this the correct smaller size (2^250), or are we using something else? *).
-  Context (ops : x86.instructions n) (props : x86.arithmetic ops).
-  Local Notation W := (tuple (tuple x86.W 2) 2) (* 256-bit words *).
-  Let ops' ldi_modulus ldi_0 : ZBounded.ZLikeOps (2^256) (2^smaller_bound_exp) modulus
-    := (@ZLikeOps_of_x86_64_Factored ops modulus smaller_bound_exp ldi_modulus ldi_0).
-  Local Existing Instance ops'.
-  Lemma modulus_in_range : (0 <= modulus < 2 ^ 256)%Z. Proof. vm_compute; intuition congruence. Qed.
   Lemma smaller_bound_smaller : (0 <= smaller_bound_exp <= 256)%Z. Proof. vm_compute; intuition congruence. Qed.
-  Lemma n_pos : (0 < 64%nat)%Z. Proof. vm_compute; intuition congruence. Qed.
+  Lemma modulusv_in_range : 0 <= modulusv < 2 ^ 256. Proof. vm_compute; intuition congruence. Qed.
+  Lemma modulusv_pos : 0 < modulusv. Proof. vm_compute; reflexivity. Qed.
+  Section gen_modulus.
+    Context (modulus : Z)
+            (modulus_in_range : 0 <= modulus < 2 ^ 256)
+            (modulus_pos : 0 < modulus).
+    Section gen.
+      Context (half_n : nat).
+      Local Notation n := (2 * half_n)%nat.
+      Context (ops : x86.instructions n) (props : x86.arithmetic ops)
+              (n_pos : (0 < n)%Z)
+              (n_pow_2 : (2^Nat.log2 (256 / n) = 256)%Z).
+      Let ops' ldi_modulus ldi_0 : ZBounded.ZLikeOps (2^256) (2^smaller_bound_exp) modulus
+        := (@ZLikeOps_of_x86_gen_Factored 256 n ops modulus smaller_bound_exp ldi_modulus ldi_0).
+      Local Existing Instance ops'.
+      Lemma full_width_pos : (0 < 256)%Z. Proof. omega. Qed.
+      Let props' ldi_modulus ldi_0 Hldi_modulus Hldi_0 : ZBounded.ZLikeProperties (ops' ldi_modulus ldi_0)
+        := @ZLikeProperties_of_x86_gen_Factored half_n 256 ops modulus props ldi_modulus ldi_0 Hldi_modulus Hldi_0 modulus_in_range smaller_bound_exp smaller_bound_smaller n_pos full_width_pos.
+      Local Existing Instance props'.
 
-  Let props' ldi_modulus ldi_0 Hldi_modulus Hldi_0 : ZBounded.ZLikeProperties (ops' ldi_modulus ldi_0)
-    := @ZLikeProperties_of_x86_64_Factored ops modulus props ldi_modulus ldi_0 Hldi_modulus Hldi_0 modulus_in_range smaller_bound_exp smaller_bound_smaller n_pos.
-  Local Existing Instance props'.
+      Let offset'0 := Eval compute in ((256 - smaller_bound_exp) / 2)%Z.
+      Let k'0 := Eval compute in ((256 - offset'0) / Z.log2 base)%Z.
+      Section params_gen.
+        Import BarrettBundled.
+        Context (ldi_modulus ldi_0 ldi_μ0 : Core.repeated_tuple x86.W 2 (Nat.log2 (256 / n))).
+        Let offset' := Eval compute in offset'0.
+        Let k' := Eval compute in k'0.
+        Local Instance x86_25519_Barrett : BarrettParameters
+          := { m := modulus;
+               b := base;
+               k := k';
+               offset := offset';
+               ops := ops' ldi_modulus ldi_0;
+               μ' := ldi_μ0 }.
+        Local Instance x86_25519_BarrettProofs
+              Hldi_modulus Hldi_0
+              (Hldi_μ0 : ldi_μ0 = ldi μ)
+              (H0 : 3 * m <= b ^ (k + offset))
+              (H1 : b ^ (k - offset) <= m + 1)
+              (H2 : (0 <= μ < 2 ^ (256 * n)))
+          : BarrettParametersCorrect x86_25519_Barrett
+          := { props := props' ldi_modulus ldi_0 Hldi_modulus Hldi_0 }.
+        Proof.
+          assumption.
+          vm_compute; reflexivity.
+          vm_compute; clear; abstract intuition congruence.
+          vm_compute; clear; abstract intuition congruence.
+          assumption.
+          assumption.
+          vm_compute; exact I.
+          { cbv [μ μ' x86_25519_Barrett ZBounded.decode_small BarrettBundled.ops ops' ZLikeOps_of_x86_64 ZLikeOps_of_x86_64_Factored ZLikeOps_of_x86_gen_Factored ZBounded.SmallT b k m offset].
+            clear -props Hldi_μ0 n_pow_2 n_pos H2.
+            abstract (
+                subst ldi_μ0;
+                assert (2^256 <= 2^(256 * n)) by (clear -n_pos; auto with zarith omega);
+                rewrite decode_load_immediate
+                  by solve [ exact _ | change (Z.of_nat 2) with 2%Z; rewrite n_pow_2; exact H2 ];
+                reflexivity
+              ). }
+        Defined.
+      End params_gen.
+      Local Existing Instance x86_25519_Barrett.
+      Local Existing Instance x86_25519_BarrettProofs.
+      Definition ldi_modulus : Core.repeated_tuple x86.W 2 (Nat.log2 (256 / n)) := ldi modulus.
+      Definition ldi_mod_inverse : Core.repeated_tuple x86.W 2 (Nat.log2 (256 / n)) := ldi (base^(2*k'0) / modulus).
+      Definition barrett_reduce (x : tuple (Core.repeated_tuple x86.W 2 (Nat.log2 (256 / n))) 2)
+        : Core.repeated_tuple x86.W 2 (Nat.log2 (256 / n))
+        := dlet ldi_modulus' := ldi_modulus in
+           dlet ldi_μ := ldi_mod_inverse in
+           dlet ldi_0 := ldi 0 in
+           let params := x86_25519_Barrett ldi_modulus' ldi_0 ldi_μ in
+           barrett_reduce_function_bundled x.
+    End gen.
+
+    Section x86_64.
+      Local Notation n := 64%nat.
+      Context (ops : x86.instructions n) (props : x86.arithmetic ops).
+      Lemma n64_pos : (0 < n)%Z. Proof. vm_compute; intuition congruence. Qed.
+      Local Notation W := (tuple (tuple x86.W 2) 2) (* 256-bit words *).
+      Local Arguments barrett_reduce / _ _ _ .
+      Local Arguments Core.load_immediate_double / _ _ _ _.
+      Local Arguments Z.modulo !_ !_ / .
+      Local Arguments Z.pow_pos !_ !_ / .
+      Local Arguments Z.div !_ !_ / .
+      Local Arguments barrett_reduce_function_bundled : simpl never.
+      Local Opaque Let_In ldi_mod_inverse ldi_modulus.
+      Definition barrett_reduce64'0 (x : tuple W 2) : W
+        := Eval simpl in @barrett_reduce 32 ops x.
+      Definition barrett_reduce64'1 (x : tuple W 2) : W
+        := Eval cbv [barrett_reduce64'0 tuple barrett_reduce_function_bundled barrett_reduce_function
+                                        BarrettBundled.m BarrettBundled.b BarrettBundled.k BarrettBundled.offset BarrettBundled.μ' x86_25519_Barrett
+                                        ZBounded.ConditionalSubtractModulus ZBounded.CarrySubSmall ZBounded.Mod_SmallBound ZBounded.Mod_SmallBound ZBounded.Mul ZBounded.DivBy_SmallBound ZBounded.DivBy_SmallerBound ZBounded.modulus_digits ZBounded.SmallT ZBounded.LargeT BarrettBundled.ops ZLikeOps_of_x86_gen_Factored]
+          in @barrett_reduce64'0 x.
+      Local Opaque Core.sub_with_carry_repeated_double Core.multiply_double_repeated_double
+            Core.shift_right_doubleword_immediate_repeated_double
+            Core.select_conditional_repeated_double.
+      Local Arguments barrett_reduce64'1 / _.
+      Definition barrett_reduce64'2 (x : tuple W 2) : W
+        := Eval simpl in @barrett_reduce64'1 x.
+      Local Transparent Core.sub_with_carry_repeated_double Core.multiply_double_repeated_double
+            Core.shift_right_doubleword_immediate_repeated_double
+            Core.select_conditional_repeated_double.
+      Definition barrett_reduce64'3 (x : tuple W 2) : W
+        := Eval cbv [barrett_reduce64'2
+                       tuple Core.repeated_tuple tuple'
+                       fst snd (*Interface.ldi Interface.muldw Interface.mulhwll Interface.mulhwhl Interface.mulhwhh Interface.shl Interface.shr Interface.or Interface.shrd Interface.adc Interface.subc Interface.selc Interface.adc Interface.subc*)
+                       Core.select_conditional_repeated_double Core.selc_double Core.select_conditional_double
+                       Core.multiply_double_repeated_double Core.mul_double_multiply Core.mul_double Core.mul_double_multiply_low_low Core.mul_double_multiply_high_low Core.mul_double_multiply_high_high
+                       Core.shift_right_doubleword_immediate_repeated_double Core.shift_right_doubleword_immediate_double Core.shrd_double
+                       Core.shlr_repeated_double
+                       Core.shift_right_immediate_repeated_double Core.shift_left_immediate_repeated_double
+                       Core.shift_left_immediate_double Core.shift_right_immediate_double
+                       Core.shr_double Core.shl_double
+                       Core.load_immediate_repeated_double Core.ldi_double Core.load_immediate_double
+                       Core.ripple_carry_adc Core.ripple_carry_subc
+                       Core.sub_with_carry_repeated_double Core.add_with_carry_repeated_double
+                       Core.ripple_carry_tuple
+                       Core.ripple_carry_tuple'
+                       StripCF.shift_left_immediate_strip_CF StripCF.shift_right_immediate_strip_CF StripCF.bitwise_or_strip_CF StripCF.shift_right_doubleword_immediate_strip_CF StripCF.multiply_double_strip_CF
+                       Nat.log2 Nat.div Nat.divmod Nat.pred Nat.log2_iter Core.ripple_carry_subc Z.of_nat Pos.of_succ_nat Z.pow Z.mul Z.eqb Z_eq_bool Pos.eqb Z.sub Z.ltb Z.leb Z.gtb Z.geb Pos.sub Pos.ltb Pos.leb Z.opp Z.add Pos.add Z.compare Z.pos_sub  Pos.compare Pos.compare_cont Z.pow_pos Pos.succ Pos.mul Pos.iter Z.succ_double Z.double Pos.pred_double
+                       Z.modulo Z.div Z.div_eucl Z.pos_div_eucl Z.ltb Z.leb Pos.ltb Pos.leb Pos.compare Pos.compare_cont Z.compare fst snd Z.mul Pos.mul Z.add Pos.add Z.sub Z.opp Z.pos_sub Z.double Z.succ_double Pos.pred_double Z.pred_double]
+          in @barrett_reduce64'2 x.
+      Local Arguments barrett_reduce64'3 / _ .
+    End x86_64.
+  End gen_modulus.
+
+  Section x86_64.
+    Local Notation n := 64%nat.
+      Context (ops : x86.instructions n) (props : x86.arithmetic ops).
+      Local Notation W := (tuple (tuple x86.W 2) 2) (* 256-bit words *).
+
+      Definition barrett_reduce64'4 (x : tuple W 2) : W.
+      Proof.
+        let ldiμ := constr:(ldi_mod_inverse modulusv 32 ops) in
+        let ldiμv := (eval cbv [ldi_mod_inverse Core.load_immediate_repeated_double Core.ldi_double Core.load_immediate_double Nat.mul Nat.add Nat.div fst PeanoNat.Nat.divmod Nat.log2 Nat.log2_iter Nat.pred Core.repeated_tuple tuple tuple'] in ldiμ) in
+        let arg := constr:(@Interface.ldi x86.W x86.ldi) in
+        let ldiμv := match (eval pattern arg in ldiμv) with ?P _ => P end in
+        let ldiμv := (eval vm_compute in ldiμv) in
+        let ldiμv := (eval cbv beta in (ldiμv arg)) in
+        let ldimodulus := constr:(ldi_modulus modulusv 32 ops) in
+        let ldimodulusv := (eval cbv [ldi_modulus Core.load_immediate_repeated_double Core.ldi_double Core.load_immediate_double Nat.mul Nat.add Nat.div fst PeanoNat.Nat.divmod Nat.log2 Nat.log2_iter Nat.pred Core.repeated_tuple tuple tuple'] in ldimodulus) in
+        let arg := constr:(@Interface.ldi x86.W x86.ldi) in
+        let ldimodulusv := match (eval pattern arg in ldimodulusv) with ?P _ => P end in
+        let ldimodulusv := (eval vm_compute in ldimodulusv) in
+        let ldimodulusv := (eval cbv beta in (ldimodulusv arg)) in
+        let v := constr:(@barrett_reduce64'3 modulusv ops x) in
+        let v := (eval cbv beta iota delta [barrett_reduce64'3] in v) in
+        let v := match v with
+                 | appcontext v'[ldiμ]
+                   => let v := context v'[ldiμv] in
+                      v
+                 end in
+        let v := match v with
+                 | appcontext v'[ldimodulus]
+                   => let v := context v'[ldimodulusv] in
+                      v
+                 end in
+        pose v.
+        ldi_modulus modulusv 32 ops
+        let
+          := Eval simpl in @barrett_reduce64'3 x.
+      Print barrett_reduce64.
+
+
+      Eval cbv [ barrett_reduce64'3  fst snd    ] in barrett_reduce64'3.
+
+      StripCF.multiply_double_strip_CF
+      Lemma modulus_in_range : (0 <= modulus < 2 ^ 256)%Z. Proof. vm_compute; intuition congruence. Qed.
+
 
   Section params_64.
     Import BarrettBundled.
