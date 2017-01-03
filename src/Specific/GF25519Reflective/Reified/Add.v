229d252a2ecf8c91a9236c65800cf121f92ea241
Jason Gross <jgross@mit.edu>
2017-01-03 16:25:15 -0500

    WIP

--- src/Specific/GF25519Reflective/Reified/Add.v
+++ src/Specific/GF25519Reflective/Reified/Add.v
@@ -1,9 +1,21 @@
 Require Export Crypto.Specific.GF25519Reflective.Common.
+Require Export Crypto.Reflection.Z.Interpretations.
 (*Require Import Crypto.Specific.GF25519Reflective.CommonBinOp.*)
 
 Definition raddZ_sig : rexpr_binop_sig add. Proof. reify_sig. Defined.
 Import Reflection.Syntax.
 Compute proj1_sig raddZ_sig.
+Require Import Reflection.MapCastWithCastOp.
+Goal True.
+  pose (proj1_sig raddZ_sig) as e.
+  let T := type of e in
+  vm_compute in e;
+    change T in (type of e).
+  refine (let k := @map_interp_cast_with_cast_op
+                     base_type base_type interp_base_type  in _).
+
+  let T := type of e in set (T' := T) in e.
+
 Definition raddW := Eval vm_compute in rword_of_Z raddZ_sig.
 Lemma raddW_correct_and_bounded_gen : correct_and_bounded_genT raddW raddZ_sig.
 Proof. rexpr_correct. Qed.
