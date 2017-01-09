ad5aff33f15a3473514a989932b8b3f034e90ded
Jason Gross <jgross@mit.edu>
2017-01-09 10:37:34 -0500

    Update notations

--- src/Reflection/Z/CNotations.v
+++ src/Reflection/Z/CNotations.v
@@ -1,16 +1,21 @@
-Require Import Crypto.SpecificGen.GF25519_64Reflective.Reified.LadderStep.
 Require Export Crypto.Reflection.Syntax.
-Require Export Bedrock.Word.
+Require Import Crypto.Reflection.Z.Syntax.
+Require Import Bedrock.Word.
 Require Export Crypto.Util.Notations.
+
+Reserved Notation "T x = A ; b" (at level 200, b at level 200, format "T  x  =  A ; '//' b").
+Reserved Notation "'(bool)' x" (at level 200, x at level 9).
+Reserved Notation "'(unsigned short)' x" (at level 200, x at level 9).
+Reserved Notation "'(uint8_t)' x" (at level 200, x at level 9).
+Reserved Notation "'(uint16_t)' x" (at level 200, x at level 9).
+Reserved Notation "'(uint32_t)' x" (at level 200, x at level 9).
+Reserved Notation "'(uint64_t)' x" (at level 200, x at level 9).
+Reserved Notation "'(uint128_t)' x" (at level 200, x at level 9).
+Reserved Notation "x & y" (at level 40).
 
 Local Open Scope expr_scope.
 
-Notation "'slet' x : T := A 'in' b" := (LetIn (tx:=T) A (fun x => b))
-                                         (at level 200, b at level 200, format "'slet'  x  :  T  :=  A  'in' '//' b")
-                                       : expr_scope.
-Notation "T x = A ; b" := (LetIn (tx:=T) A (fun x => b))
-                                         (at level 200, b at level 200, format "T  x  =  A ; '//' b")
-                                       : expr_scope.
+Notation "T x = A ; b" := (LetIn (tx:=T) A (fun x => b)) : expr_scope.
 (*Notation uint32_t := (_ (TWord 5)).
 Notation uint64_t := (_ (TWord 6)).
 Notation uint128_t := (_ (TWord 7)).*)
@@ -22,22 +27,22 @@ Notation uint16_t := (Tbase (TWord 4)).
 Notation uint32_t := (Tbase (TWord 5)).
 Notation uint64_t := (Tbase (TWord 6)).
 Notation uint128_t := (Tbase (TWord 7)).
-Notation "'(bool)' x" := (Op (Cast _ (TWord 0)) x) (at level 200, x at level 9).
-Notation "'(unsigned short)' x" := (Op (Cast _ (TWord 1)) x) (at level 200, x at level 9).
-Notation "'(unsigned short)' x" := (Op (Cast _ (TWord 2)) x) (at level 200, x at level 9).
-Notation "'(uint8_t)' x" := (Op (Cast _ (TWord 3)) x) (at level 200, x at level 9).
-Notation "'(uint16_t)' x" := (Op (Cast _ (TWord 4)) x) (at level 200, x at level 9).
-Notation "'(uint32_t)' x" := (Op (Cast _ (TWord 5)) x) (at level 200, x at level 9).
-Notation "'(uint64_t)' x" := (Op (Cast _ (TWord 6)) x) (at level 200, x at level 9).
-Notation "'(uint128_t)' x" := (Op (Cast _ (TWord 7)) x) (at level 200, x at level 9).
-Notation "'(bool)' x" := (Op (Cast _ (TWord 0)) (Var x)) (at level 200, x at level 9).
-Notation "'(unsigned short)' x" := (Op (Cast _ (TWord 1)) (Var x)) (at level 200, x at level 9).
-Notation "'(unsigned short)' x" := (Op (Cast _ (TWord 2)) (Var x)) (at level 200, x at level 9).
-Notation "'(uint8_t)' x" := (Op (Cast _ (TWord 3)) (Var x)) (at level 200, x at level 9).
-Notation "'(uint16_t)' x" := (Op (Cast _ (TWord 4)) (Var x)) (at level 200, x at level 9).
-Notation "'(uint32_t)' x" := (Op (Cast _ (TWord 5)) (Var x)) (at level 200, x at level 9).
-Notation "'(uint64_t)' x" := (Op (Cast _ (TWord 6)) (Var x)) (at level 200, x at level 9).
-Notation "'(uint128_t)' x" := (Op (Cast _ (TWord 7)) (Var x)) (at level 200, x at level 9).
+Notation "'(bool)' x" := (Op (Cast _ (TWord 0)) x).
+Notation "'(unsigned short)' x" := (Op (Cast _ (TWord 1)) x).
+Notation "'(unsigned short)' x" := (Op (Cast _ (TWord 2)) x).
+Notation "'(uint8_t)' x" := (Op (Cast _ (TWord 3)) x).
+Notation "'(uint16_t)' x" := (Op (Cast _ (TWord 4)) x).
+Notation "'(uint32_t)' x" := (Op (Cast _ (TWord 5)) x).
+Notation "'(uint64_t)' x" := (Op (Cast _ (TWord 6)) x).
+Notation "'(uint128_t)' x" := (Op (Cast _ (TWord 7)) x).
+Notation "'(bool)' x" := (Op (Cast _ (TWord 0)) (Var x)).
+Notation "'(unsigned short)' x" := (Op (Cast _ (TWord 1)) (Var x)).
+Notation "'(unsigned short)' x" := (Op (Cast _ (TWord 2)) (Var x)).
+Notation "'(uint8_t)' x" := (Op (Cast _ (TWord 3)) (Var x)).
+Notation "'(uint16_t)' x" := (Op (Cast _ (TWord 4)) (Var x)).
+Notation "'(uint32_t)' x" := (Op (Cast _ (TWord 5)) (Var x)).
+Notation "'(uint64_t)' x" := (Op (Cast _ (TWord 6)) (Var x)).
+Notation "'(uint128_t)' x" := (Op (Cast _ (TWord 7)) (Var x)).
 Notation "x + y" := (Op (Add _) (Pair x y)).
 Notation "x + y" := (Op (Add _) (Pair (Var x) y)).
 Notation "x + y" := (Op (Add _) (Pair x (Var y))).
@@ -119,7 +124,7 @@ Notation "x >> y" := (Op (Shr _) (Pair x (Op (Cast _ _) y))).
 Notation "x >> y" := (Op (Shr _) (Pair (Var x) (Op (Cast _ _) y))).
 Notation "x >> y" := (Op (Shr _) (Pair x (Op (Cast _ _) (Var y)))).
 Notation "x >> y" := (Op (Shr _) (Pair (Var x) (Op (Cast _ _) (Var y)))).
-Notation "x & y" := (Op (Land _) (Pair x y)) (at level 40).
+Notation "x & y" := (Op (Land _) (Pair x y)).
 Notation "x & y" := (Op (Land _) (Pair (Var x) y)).
 Notation "x & y" := (Op (Land _) (Pair x (Var y))).
 Notation "x & y" := (Op (Land _) (Pair (Var x) (Var y))).
@@ -152,18 +157,20 @@ Notation "'0b10'" := (Const WO~1~0). (* 2 *)
 Notation "'0b00010011'" := (Const WO~0~0~0~1~0~0~1~1). (* 19 *)
 Notation "'0b00011001'" := (Const WO~0~0~0~1~1~0~0~1). (* 25 *)
 Notation "'0b00011010'" := (Const WO~0~0~0~1~1~0~1~0). (* 26 *)
-Notation "'0b00000011111111111111111111111111'" := (Const WO~0~0~0~0~0~0~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1).
-Notation "'0b00000001111111111111111111111111'" := (Const WO~0~0~0~0~0~0~0~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1).
-Notation "'0b00000011111111111111111111111110'" := (Const WO~0~0~0~0~0~0~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~0).
-Notation "'0b00000111111111111111111111011010'" := (Const WO~0~0~0~0~0~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~0~1~1~0~1~0).
 Notation "'0b00110011'" := (Const WO~0~0~1~1~0~0~1~1).
+Notation "'0b00000001111111111111111111111111'"
+  := (Const WO~0~0~0~0~0~0~0~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1).
+Notation "'0b00000011111111111111111111111110'"
+  := (Const WO~0~0~0~0~0~0~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~0).
+Notation "'0b00000011111111111111111111111111'"
+  := (Const WO~0~0~0~0~0~0~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1).
+Notation "'0b00000111111111111111111111111110'"
+  := (Const WO~0~0~0~0~0~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~0).
+Notation "'0b00000111111111111111111111011010'"
+  := (Const WO~0~0~0~0~0~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~0~1~1~0~1~0).
 Notation "'0b0000000000000111111111111111111111111111111111111111111111111111'"
   := (Const WO~0~0~0~0~0~0~0~0~0~0~0~0~0~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1).
 Notation "'0b0000000000001111111111111111111111111111111111111111111111111110'"
   := (Const WO~0~0~0~0~0~0~0~0~0~0~0~0~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~0).
 Notation "'0b0000000000001111111111111111111111111111111111111111111111011010'"
   := (Const WO~0~0~0~0~0~0~0~0~0~0~0~0~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~0~1~1~0~1~0).
-Notation "'0b00000111111111111111111111111110'"
-  := (Const WO~0~0~0~0~0~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~1~0).
-Notation C_like := (Common.Expr _).
-Print rladderstepW.
