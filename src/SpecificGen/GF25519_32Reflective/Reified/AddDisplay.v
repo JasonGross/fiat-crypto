32d81a36db9b500054f6867b2af5c78ccfaa1be1
Jason Gross <jgross@mit.edu>
2017-01-09 10:49:27 -0500

    Update notations import

--- src/SpecificGen/GF25519_32Reflective/Reified/AddDisplay.v
+++ src/SpecificGen/GF25519_32Reflective/Reified/AddDisplay.v
@@ -1,17 +1,5 @@
 Require Import Crypto.SpecificGen.GF25519_32Reflective.Reified.Add.
-Require Export Crypto.Reflection.Syntax.
+Require Export Crypto.Reflection.Z.CNotations.
 
-Local Open Scope expr_scope.
-
-Notation "'slet' x : T := A 'in' b" := (LetIn (tx:=T) A (fun x => b))
-                                         (at level 200, b at level 200, format "'slet'  x  :  T  :=  A  'in' '//' b")
-                                       : expr_scope.
-Notation "T x = A ; b" := (LetIn (tx:=T) A (fun x => b))
-                                         (at level 200, b at level 200, format "T  x  =  A ; '//' b")
-                                       : expr_scope.
-Notation uint32_t := (Tbase (TWord 5)).
-Notation uint64_t := (Tbase (TWord 6)).
-Notation uint128_t := (Tbase (TWord 7)).
-Notation "x + y" := (Op (Add _) (Pair (Var x) (Var y))).
-Notation Return x := (Var x).
+Notation C_like := (Common.Expr _).
 Print raddW.
