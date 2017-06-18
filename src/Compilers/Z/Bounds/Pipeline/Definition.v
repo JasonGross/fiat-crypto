6822f3b9f35df3ee5be297559e65a5b195f44e06
Jason Gross <jgross@mit.edu>
2017-06-18 00:33:13 -0400

    a bit more simplification
    make display

--- src/Compilers/Z/Bounds/Pipeline/Definition.v
+++ src/Compilers/Z/Bounds/Pipeline/Definition.v
@@ -120,7 +120,7 @@ Definition PostWfBoundsPipeline
           (projT2_map
              (fun b e'
               => let e' := InlineConst e' in
-                 (*let e' := InlineConst (Linearize (SimplifyArith false e')) in
+                 let e' := InlineConst (Linearize (SimplifyArith false e')) in(*
                  let e' := InlineConst (Linearize (SimplifyArith false e')) in
                  let e' := InlineConst (Linearize (SimplifyArith false e')) in
                  let e' := InlineConst (Linearize (SimplifyArith false e')) in
@@ -128,8 +128,8 @@ Definition PostWfBoundsPipeline
                  let e' := InlineConst (Linearize (SimplifyArith false e')) in
                  let e' := InlineConst (Linearize (SimplifyArith false e')) in
                  let e' := InlineConst (Linearize (SimplifyArith false e')) in
-                 let e' := InlineConst (Linearize (SimplifyArith false e')) in
-                 let e' := if opts.(anf) then InlineConst (ANormal e') else e' in*)
+                 let e' := InlineConst (Linearize (SimplifyArith false e')) in*)
+                 let e' := if opts.(anf) then InlineConst (ANormal e') else e' in
                  let e' := ExprEta e' in
                  e'))
           e).
