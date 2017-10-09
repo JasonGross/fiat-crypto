b1e2e3ede2f2e92577e528a57eb1b0a571ccf59a
Jason Gross <jgross@mit.edu>
2017-10-09 14:32:44 -0400

    Add TagList
    List of dynamically-typed key-value pairs, and some helper definitions
    and tactics.

---
+++ src/Util/TagList.v
@@ -0,0 +1,26 @@
+Require Import Coq.Lists.List.
+Import ListNotations.
+
+Module Tag.
+  Record tagged :=
+    { keyT : Type;
+      key : keyT;
+      valueT : Type;
+      value : valueT }.
+
+  Definition Context := list tagged.
+
+  Definition add {K V} (key' : K) (value' : V) (ctx : Context) : Context
+    := cons {| key := key' ; value := value' |} ctx.
+
+  Ltac update ctx key value :=
+    constr:(add key value ctx).
+
+  Ltac get ctx key' :=
+    lazymatch (eval hnf in ctx) with
+    | context[{| key := key' ; value := ?value' |}]
+      => value'
+    end.
+
+  Notation get ctx key' := ltac:(let v := get ctx key' in exact v) (only parsing).
+End Tag.
