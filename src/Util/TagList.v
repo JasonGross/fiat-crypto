cf043d31862e2d8893dc1baae85d20812c4a7da3
Jason Gross <jgross@mit.edu>
2017-10-09 18:48:38 -0400

    Add local updates to context

--- src/Util/TagList.v
+++ src/Util/TagList.v
@@ -6,20 +6,42 @@ Module Tag.
     { keyT : Type;
       key : keyT;
       valueT : Type;
-      value : valueT }.
+      value : valueT;
+      local : bool }.
 
   Definition Context := list tagged.
 
+  Definition add_gen {K V} (key' : K) (value' : V) (local' : bool) (ctx : Context) : Context
+    := cons {| key := key' ; value := value' ; local := local' |} ctx.
+
   Definition add {K V} (key' : K) (value' : V) (ctx : Context) : Context
-    := cons {| key := key' ; value := value' |} ctx.
+    := add_gen key' value' false ctx.
+
+  Definition add_local {K V} (key' : K) (value' : V) (ctx : Context) : Context
+    := add_gen key' value' true ctx.
+
+  Definition strip_local (ctx : Context) : Context
+    := List.filter (fun '{| key := k ; value := v ; local := loc |}
+                    => negb loc)
+                   ctx.
+
+  Ltac strip_local ctx :=
+    let upd := (eval cbv [strip_local negb List.filter] in strip_local) in
+    eval cbv [add add_local add_gen] in (upd ctx).
 
   Ltac update ctx key value :=
     constr:(add key value ctx).
+
+  Ltac local_update ctx key value :=
+    constr:(add_local key value ctx).
 
   Ltac get_gen ctx key' default :=
     lazymatch (eval hnf in ctx) with
     | context[{| key := key' ; value := ?value' |}]
       => value'
+    | context[add_gen ?key' ?value' _ _] => value'
+    | context[add_local ?key' ?value' _] => value'
+    | context[add ?key' ?value' _] => value'
     | _ => default ()
     end.
 
@@ -27,6 +49,9 @@ Module Tag.
     lazymatch (eval hnf in ctx) with
     | context[{| key := key' ; value := ?value' |}]
       => value'
+    | context[add_gen ?key' ?value' _ _] => value'
+    | context[add_local ?key' ?value' _] => value'
+    | context[add ?key' ?value' _] => value'
     end.
 
   Ltac get ctx key' := get_gen ctx key' ltac:(fun _ => constr:(I)).
