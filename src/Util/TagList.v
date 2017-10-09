420b8acd355a092c0907a421f6bcbabc201e117b
Jason Gross <jgross@mit.edu>
2017-10-09 15:46:00 -0400

    Return default value on failed get in taglist

--- src/Util/TagList.v
+++ src/Util/TagList.v
@@ -16,11 +16,20 @@ Module Tag.
   Ltac update ctx key value :=
     constr:(add key value ctx).
 
-  Ltac get ctx key' :=
+  Ltac get_gen ctx key' default :=
+    lazymatch (eval hnf in ctx) with
+    | context[{| key := key' ; value := ?value' |}]
+      => value'
+    | _ => default ()
+    end.
+
+  Ltac get_error ctx key' :=
     lazymatch (eval hnf in ctx) with
     | context[{| key := key' ; value := ?value' |}]
       => value'
     end.
+
+  Ltac get ctx key' := get_gen ctx key' ltac:(fun _ => constr:(I)).
 
   Notation get ctx key' := ltac:(let v := get ctx key' in exact v) (only parsing).
 
