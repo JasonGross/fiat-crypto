b38b8de6a89592a8a84776a1d9d70e03f97fe0ac
Jason Gross <jgross@mit.edu>
2017-10-13 01:37:52 -0400

    WIP

--- src/Specific/Framework/OutputType.v
+++ src/Specific/Framework/OutputType.v
@@ -14,15 +14,15 @@ Import CurveParameters.Notations.
 Local Coercion Z.to_nat : Z >-> nat.
 Local Notation interp_flat_type := (interp_flat_type interp_base_type).
 
-Record SynthesisOutput (curve : RawCurveParameters.CurveParameters) :=
-  {
-    m := Z.to_pos (curve.(s) - Associational.eval curve.(c))%Z;
-    rT := ((Tbase (TWord (Z.log2_up curve.(bitwidth))))^curve.(sz))%ctype;
-    T' := interp_flat_type rT;
-    RT := (Unit -> rT)%ctype;
-    wt := wt_gen m curve.(sz);
+Section with_curve.
+  Context (curve : RawCurveParameters.CurveParameters).
 
-    encode : F m -> Expr RT
+  Definition m := Z.to_pos (curve.(s) - Associational.eval curve.(c))%Z.
+  Definition rT := ((Tbase (TWord (Z.log2_up curve.(bitwidth))))^curve.(sz))%ctype.
+  Definition T' := (interp_flat_type rT).
+  Definition RT := (Unit -> rT)%ctype.
+  Definition wt := (wt_gen m curve.(sz)).
+  Definition encode : F m -> Expr RT
     := fun v var
        => Abs
             (fun _
@@ -31,66 +31,70 @@ Record SynthesisOutput (curve : RawCurveParameters.CurveParameters) :=
                      (T:=Tbase _)
                      (Tuple.map
                         (fun v => Op (OpConst v) TT)
-                        (@Positional.Fencode wt curve.(sz) m modulo div v))));
-    decode : T' -> F m
+                        (@Positional.Fencode wt curve.(sz) m modulo div v)))).
+  Definition decode : T' -> F m
     := fun v => @Positional.Fdecode
                   wt (curve.(sz)) m
-                  (Tuple.map interpToZ (flat_interp_tuple (T:=Tbase _) v));
-    zero : Expr RT;
-    one : Expr RT;
-    add : Expr (rT * rT -> rT); (* does not include carry *)
-    sub : Expr (rT * rT -> rT); (* does not include carry *)
-    mul : Expr (rT * rT -> rT); (* includes carry *)
-    square : Expr (rT -> rT); (* includes carry *)
-    opp : Expr (rT -> rT); (* does not include carry *)
-    carry : Expr (rT -> rT);
-    carry_add : Expr (rT * rT -> rT)
-    := (carry ∘ add)%expr;
-    carry_sub : Expr (rT * rT -> rT)
-    := (carry ∘ sub)%expr;
-    carry_opp : Expr (rT -> rT)
-    := (carry ∘ opp)%expr;
+                  (Tuple.map interpToZ (flat_interp_tuple (T:=Tbase _) v)).
 
-    P : T' -> Prop;
+  Record SynthesisOutput :=
+    {
+      zero : Expr RT;
+      one : Expr RT;
+      add : Expr (rT * rT -> rT); (* does not include carry *)
+      sub : Expr (rT * rT -> rT); (* does not include carry *)
+      mul : Expr (rT * rT -> rT); (* includes carry *)
+      square : Expr (rT -> rT); (* includes carry *)
+      opp : Expr (rT -> rT); (* does not include carry *)
+      carry : Expr (rT -> rT);
+      carry_add : Expr (rT * rT -> rT)
+      := (carry ∘ add)%expr;
+      carry_sub : Expr (rT * rT -> rT)
+      := (carry ∘ sub)%expr;
+      carry_opp : Expr (rT -> rT)
+      := (carry ∘ opp)%expr;
 
-    encode_valid : forall v, _;
-    encode_sig := fun v => exist P (Interp (encode v) tt) (encode_valid v);
-    encode_correct : forall v, decode (Interp (encode v) tt) = v;
+      P : T' -> Prop;
 
-    decode_sig := fun v : sig P => decode (proj1_sig v);
+      encode_valid : forall v, _;
+      encode_sig := fun v => exist P (Interp (encode v) tt) (encode_valid v);
+      encode_correct : forall v, decode (Interp (encode v) tt) = v;
 
-    zero_correct : zero = encode 0%F; (* which equality to use here? *)
-    one_correct : one = encode 1%F; (* which equality to use here? *)
-    zero_sig := encode_sig 0%F;
-    one_sig := encode_sig 1%F;
+      decode_sig := fun v : sig P => decode (proj1_sig v);
 
-    opp_valid : forall x, P x -> P (Interp carry_opp x);
-    opp_sig := fun x => exist P _ (@opp_valid (proj1_sig x) (proj2_sig x));
+      zero_correct : zero = encode 0%F; (* which equality to use here? *)
+      one_correct : one = encode 1%F; (* which equality to use here? *)
+      zero_sig := encode_sig 0%F;
+      one_sig := encode_sig 1%F;
 
-    add_valid : forall x y, P x -> P y -> P (Interp carry_add (x, y));
-    add_sig := fun x y => exist P _ (@add_valid (proj1_sig x) (proj1_sig y) (proj2_sig x) (proj2_sig y));
+      opp_valid : forall x, P x -> P (Interp carry_opp x);
+      opp_sig := fun x => exist P _ (@opp_valid (proj1_sig x) (proj2_sig x));
 
-    sub_valid : forall x y, P x -> P y -> P (Interp carry_sub (x, y));
-    sub_sig := fun x y => exist P _ (@sub_valid (proj1_sig x) (proj1_sig y) (proj2_sig x) (proj2_sig y));
+      add_valid : forall x y, P x -> P y -> P (Interp carry_add (x, y));
+      add_sig := fun x y => exist P _ (@add_valid (proj1_sig x) (proj1_sig y) (proj2_sig x) (proj2_sig y));
 
-    mul_valid : forall x y, P x -> P y -> P (Interp mul (x, y));
-    mul_sig := fun x y => exist P _ (@mul_valid (proj1_sig x) (proj1_sig y) (proj2_sig x) (proj2_sig y));
+      sub_valid : forall x y, P x -> P y -> P (Interp carry_sub (x, y));
+      sub_sig := fun x y => exist P _ (@sub_valid (proj1_sig x) (proj1_sig y) (proj2_sig x) (proj2_sig y));
 
-    square_correct : forall x, P x -> Interp square x = Interp mul (x, x);
+      mul_valid : forall x y, P x -> P y -> P (Interp mul (x, y));
+      mul_sig := fun x y => exist P _ (@mul_valid (proj1_sig x) (proj1_sig y) (proj2_sig x) (proj2_sig y));
 
-    T := { v : T' | P v };
-    eqT : T -> T -> Prop
-    := fun x y => eq (decode (proj1_sig x)) (decode (proj1_sig y));
-    ring : @Hierarchy.ring
-             T eqT zero_sig one_sig opp_sig add_sig sub_sig mul_sig;
-    encode_homomorphism
-    : @Ring.is_homomorphism
-        (F m) eq 1%F F.add F.mul
-        T eqT one_sig add_sig mul_sig
-        encode_sig;
-    decode_homomorphism
-    : @Ring.is_homomorphism
-        T eqT one_sig add_sig mul_sig
-        (F m) eq 1%F F.add F.mul
-        decode_sig
+      square_correct : forall x, P x -> Interp square x = Interp mul (x, x);
+
+      T := { v : T' | P v };
+      eqT : T -> T -> Prop
+      := fun x y => eq (decode (proj1_sig x)) (decode (proj1_sig y));
+      ring : @Hierarchy.ring
+               T eqT zero_sig one_sig opp_sig add_sig sub_sig mul_sig;
+      encode_homomorphism
+      : @Ring.is_homomorphism
+          (F m) eq 1%F F.add F.mul
+          T eqT one_sig add_sig mul_sig
+          encode_sig;
+      decode_homomorphism
+      : @Ring.is_homomorphism
+          T eqT one_sig add_sig mul_sig
+          (F m) eq 1%F F.add F.mul
+          decode_sig
   }.
+End with_curve.
---
+++ src/Specific/Framework/Pipeline.v
@@ -0,0 +1,86 @@
+Require Import Crypto.Arithmetic.Core. Import B.
+Require Import Crypto.Arithmetic.PrimeFieldTheorems.
+Require Import Crypto.Compilers.Syntax.
+Require Import Crypto.Compilers.SmartMap.
+Require Import Crypto.Compilers.Tuple.
+Require Import Crypto.Compilers.ExprInversion.
+Require Import Crypto.Compilers.Z.Syntax.Util.
+Require Import Crypto.Compilers.Z.Syntax.
+Require Import Crypto.Specific.Framework.RawCurveParameters.
+Require Import Crypto.Specific.Framework.ArithmeticSynthesis.Base.
+Require Import Crypto.Specific.Framework.OutputType.
+Require Import Crypto.Util.Notations.
+Import CurveParameters.Notations.
+
+Local Coercion Z.to_nat : Z >-> nat.
+Local Notation interp_flat_type := (interp_flat_type interp_base_type).
+
+Section with_curve.
+  Context (curve : RawCurveParameters.CurveParameters).
+
+  Local Notation RT := (RT curve).
+  Local Notation rT := (rT curve).
+  Local Notation T' := (T' curve).
+  Local Notation encode := (encode curve).
+  Local Notation decode := (decode curve).
+
+  Record SynthesisPreOutput :=
+    {
+      zero : Expr RT;
+      one : Expr RT;
+      add : Expr (rT * rT -> rT); (* does not include carry *)
+      sub : Expr (rT * rT -> rT); (* does not include carry *)
+      mul : Expr (rT * rT -> rT); (* includes carry *)
+      square : Expr (rT -> rT); (* includes carry *)
+      opp : Expr (rT -> rT); (* does not include carry *)
+      carry : Expr (rT -> rT);
+      carry_add : Expr (rT * rT -> rT)
+      := (carry ∘ add)%expr;
+      carry_sub : Expr (rT * rT -> rT)
+      := (carry ∘ sub)%expr;
+      carry_opp : Expr (rT -> rT)
+      := (carry ∘ opp)%expr;
+
+      P : T' -> Prop;
+
+      encode_valid : forall v, P (Interp (encode v) tt);
+
+      zero_correct : zero = encode 0%F; (* which equality to use here? *)
+      one_correct : one = encode 1%F; (* which equality to use here? *)
+
+      opp_valid : forall x, P x -> P (Interp carry_opp x);
+      add_valid : forall x y, P x -> P y -> P (Interp carry_add (x, y));
+      sub_valid : forall x y, P x -> P y -> P (Interp carry_sub (x, y));
+      mul_valid : forall x y, P x -> P y -> P (Interp mul (x, y));
+      square_correct : forall x, P x -> Interp square x = Interp mul (x, x);
+    }.
+
+  Definition FinalizeOutput (v : SynthesisPreOutput) : SynthesisOutput curve.
+  Proof.
+    simple refine (let ring_goal : _ /\ _ /\ _ := _ in _); [ shelve.. | | ]; cycle 1.
+    simple refine {|
+             OutputType.zero := v.(zero);
+             OutputType.one := v.(one);
+             OutputType.add := v.(add);
+             OutputType.sub := v.(sub);
+             OutputType.mul := v.(mul);
+             OutputType.opp := v.(opp);
+             OutputType.carry := v.(carry);
+             OutputType.square := v.(square);
+
+             OutputType.zero_correct := v.(zero_correct);
+             OutputType.one_correct := v.(one_correct);
+             OutputType.encode_valid := v.(encode_valid);
+             OutputType.add_valid := v.(add_valid);
+             OutputType.sub_valid := v.(sub_valid);
+             OutputType.mul_valid := v.(mul_valid);
+             OutputType.opp_valid := v.(opp_valid);
+             OutputType.square_correct := v.(square_correct);
+
+             OutputType.ring := proj1 ring_goal;
+             OutputType.encode_homomorphism := proj1 (proj2 ring_goal);
+             OutputType.decode_homomorphism := proj2 (proj2 ring_goal);
+           |}.
+    { intro x; cbv [decode encode Interp interp].
+      SearchAbout interpf SmartPairf.
+    constructor.
