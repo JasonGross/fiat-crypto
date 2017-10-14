9a50b5ae4c1479aeebd786583950a46e9d7aaac8
Jason Gross <jgross@mit.edu>
2017-10-14 17:30:09 -0400

    add cache_vm_sig_with_type_by

--- src/Util/Tactics/CacheTerm.v
+++ src/Util/Tactics/CacheTerm.v
@@ -26,12 +26,17 @@ Ltac cache_term term id :=
   let ty := type of term in
   cache_term_with_type_by ty ltac:(exact_no_check term) id.
 
-Ltac cache_sig_with_type_by ty tac id :=
+Ltac cache_sig_red_with_type_by ty tac red_tac id :=
   let id' := fresh id in
   let id' := fresh id in
   let id' := cache_term_with_type_by ty tac id' in
   cache_term_with_type_by
     ty
     ltac:(let v := (eval cbv beta iota delta [id' proj1_sig] in (proj1_sig id')) in
+          let v := red_tac v in
           (exists v); abstract exact_no_check (proj2_sig id'))
            id.
+Ltac cache_sig_with_type_by ty tac id
+  := cache_sig_red_with_type_by ty tac ltac:(fun v => v) id.
+Ltac cache_vm_sig_with_type_by ty tac id
+  := cache_sig_red_with_type_by ty tac ltac:(fun v => eval vm_compute in v) id.
