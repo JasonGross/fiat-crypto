de1782b91eaeda26c5a8e909737c9f14ff624150
Jason Gross <jgross@mit.edu>
2017-11-04 01:04:08 -0400

    WIP

--- src/Arithmetic/Compiled/Saturated/Montgomery.v
+++ src/Arithmetic/Compiled/Saturated/Montgomery.v
@@ -10,6 +10,8 @@ Hint Unfold
      pre_redc_cps
      redc_loop_cps
      redc_body_cps
+     add sub opp nonzero
+     add_cps sub_cps opp_cps nonzero_cps
      MontgomeryAPI.T
      MontgomeryAPI.zero
      MontgomeryAPI.nonzero_cps
@@ -30,7 +32,7 @@ Definition compiled_preredc_sig (r : positive) (R_numlimbs : nat)
   : { t : _ & t }.
 Proof.
   do_compile_sig
-    (fun n m T (f : Tuple.tuple _ _ -> _) (NAB : Tuple.tuple _ _ * (Tuple.tuple _ _ * Tuple.tuple _ _)) k => @redc_cps r n (fst NAB) m (fst (snd NAB)) (snd (snd NAB)) k T f)
+    (fun n m T (f : Tuple.tuple _ _ -> _) NAB k => @redc_cps r n (fst NAB) m (fst (snd NAB)) (snd (snd NAB)) k T f)
     uconstr:(fun t xy => t R_numlimbs A_numlimbs xy k).
 Defined.
 
@@ -38,6 +40,58 @@ Definition compiled_preredc' r R_numlimbs A_numlimbs k
   := Eval cbv [projT2 projT1 compiled_preredc_sig] in
       projT2 (compiled_preredc_sig r R_numlimbs A_numlimbs k).
 
+Definition compiled_preadd_sig (r : positive) (R_numlimbs : nat)
+  : { t : _ & t }.
+Proof.
+  do_compile_sig
+    (fun n T f NAB => @add_cps r n (fst NAB) (fst (snd NAB)) (snd (snd NAB)) T f)
+    uconstr:(fun t xy => t R_numlimbs xy).
+Defined.
+
+Definition compiled_preadd' r R_numlimbs
+  := Eval cbv [projT2 projT1 compiled_preadd_sig] in
+      projT2 (compiled_preadd_sig r R_numlimbs).
+
+Definition compiled_presub_sig (r : positive) (R_numlimbs : nat)
+  : { t : _ & t }.
+Proof.
+  do_compile_sig
+    (fun n T f NAB => @sub_cps r n (fst NAB) (fst (snd NAB)) (snd (snd NAB)) T f)
+    uconstr:(fun t xy => t R_numlimbs xy).
+Defined.
+
+Definition compiled_presub' r R_numlimbs
+  := Eval cbv [projT2 projT1 compiled_presub_sig] in
+      projT2 (compiled_presub_sig r R_numlimbs).
+
+Definition compiled_preopp_sig (r : positive) (R_numlimbs : nat)
+  : { t : _ & t }.
+Proof.
+  do_compile_sig
+    (fun n T f NA => @opp_cps r n (fst NA) (snd NA) T f)
+    uconstr:(fun t xy => t R_numlimbs xy).
+Defined.
+
+Definition compiled_preopp' r R_numlimbs
+  := Eval cbv [projT2 projT1 compiled_preopp_sig] in
+      projT2 (compiled_preopp_sig r R_numlimbs).
+Check @nonzero_cps.
+Definition compiled_prenonzero_sig (R_numlimbs : nat)
+  : { t : _ & t }.
+Proof.
+  do_compile_sig
+    (fun n T (f : Tuple.tuple _ 1 -> _) A => @nonzero_cps n A T f)
+    uconstr:(fun t xy => t R_numlimbs xy).
+Defined.
+
+Definition compiled_prenonzero' R_numlimbs
+  := Eval cbv [projT2 projT1 compiled_prenonzero_sig] in
+      projT2 (compiled_prenonzero_sig R_numlimbs).
+
+Time Definition compiled_preadd := compiler_prered compiled_preadd'.
+Time Definition compiled_presub := compiler_prered compiled_presub'.
+Time Definition compiled_preopp := compiler_prered compiled_preopp'.
+Time Definition compiled_prenonzero := compiler_prered compiled_prenonzero'.
 Time Definition compiled_preredc := compiler_prered compiled_preredc'.
 
 (*
