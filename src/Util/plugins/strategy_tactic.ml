3654b3fb2dbc7f25f87fae1aee9699755ed6d5cd
Jason Gross <jgross@mit.edu>
2019-10-08 13:58:04 -0400

    WIP

---
+++ src/Util/plugins/strategy_tactic.ml
@@ -0,0 +1,4 @@
+open Genarg
+
+let wit_strategy_level : Conv_oracle.level uniform_genarg_type =
+  make0 "strategy_level"
---
+++ src/Util/plugins/strategy_tactic.mli
@@ -0,0 +1,3 @@
+open Genarg
+
+val wit_strategy_level : Conv_oracle.level uniform_genarg_type
