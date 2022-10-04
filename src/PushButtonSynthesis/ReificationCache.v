cb17654eeff9d9d814127566899a7f9dacbbec3e
Jason Gross <jgross@mit.edu>
2022-10-04 17:34:56 +0530

    [PERF ONLY] [DO NOT MERGE] Allow perf of cache_reify
    Ltac2 Set locality seems to be broken

--- src/PushButtonSynthesis/ReificationCache.v
+++ src/PushButtonSynthesis/ReificationCache.v
@@ -35,17 +35,37 @@ Notation is_reification_of rop op
       end)
        (only parsing).
 
-Ltac cache_reify _ :=
-  split;
-  [ intros;
-    etransitivity;
-    [
-    | repeat match goal with |- _ = ?f' ?x => is_var x; apply (f_equal (fun f => f _)) end;
-      Reify_rhs ();
-      reflexivity ];
-    subst_evars;
-    reflexivity
-  | prove_Wf () ].
+Require Ltac2.Ltac2.
+Module WithLtac2.
+  Import Ltac2.Ltac2.
+  Module Exports.
+    #[global,export] Ltac2 Set Reify.should_debug_profile := fun () => true.
+  End Exports.
+  Ltac2 cache_reify () :=
+    let timed_abstract_exact_no_check ce1 a1 e1 c :=
+      Control.time (Some ce1) (fun () => ltac1:(subst_evars));
+      Control.time (Some a1) (fun () => abstract (Control.time (Some e1) (fun () => Std.exact_no_check c))) in
+    let timed_abstract_reflexivity ce1 r1 a1 e1 :=
+      let c := Control.time (Some r1)
+                            (fun () =>
+                               let g := Control.goal () in
+                               constr:(match True return $g with _ => eq_refl end)) in
+      timed_abstract_exact_no_check ce1 a1 e1 c in
+    split
+    > [ intros;
+        etransitivity
+        > [
+          | Control.time (Some "repeat apply f_equal")
+                         (fun () => ltac1:(repeat match goal with [ |- _ = ?f' ?x ] => is_var x; apply (f_equal (fun f => f _)) end));
+            Control.time (Some "Reify_rhs") (fun () => ltac1:(Reify_rhs ()));
+            timed_abstract_reflexivity "subst_evars1" "reflexivity1" "abstract1" "exact_no_check1" ];
+        Control.time (Some "subst_evars1.5") (fun () => ltac1:(subst_evars));
+        timed_abstract_reflexivity "subst_evars2" "reflexivity2" "abstract2" "exact_no_check2"
+      | let c := Control.time (Some "prove_Wf") (fun () => let g := Control.goal () in constr:(match True return $g with _ => ltac:(prove_Wf ()) end)) in
+        timed_abstract_exact_no_check "subst_evars3" "abstract3" "exact_no_check3" c ].
+End WithLtac2.
+
+Ltac cache_reify _ := ltac2:(WithLtac2.cache_reify ()).
 
 Ltac apply_cached_reification op lem :=
   lazymatch goal with
@@ -59,6 +79,7 @@ Create HintDb reify_gen_cache discriminated.
 Create HintDb wf_gen_cache discriminated.
 
 Module Export Hints.
+  Export WithLtac2.Exports.
 #[global]
   Hint Resolve conj : reify_gen_cache wf_gen_cache.
 #[global]
